061697bc3ad0339e7df7fa004224a5417064f3eb
Jiawei Zhang <jiawei.zhang1@mohawkcollege.ca>
2018-09-11 22:35:34 -0400

    Empty project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Project start
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
